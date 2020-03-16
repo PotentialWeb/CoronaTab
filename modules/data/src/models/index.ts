fce86cfdaf766fe99a876b2883c2f975d5b33ef1
nodeful <romankisilo@gmail.com>
2020-03-16 12:17:59 +0000

    work on the ingestion

--- modules/data/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './locale'
-export * from './place'
-export * from './place/polygon'
-export * from './place/type'
-export * from './place/data'
--- modules/data/src/seeds/index.ts
+++
@@ -1,4 +0,0 @@
-
-export * from './locales'
-export * from './places'
-export * from './places/types'
