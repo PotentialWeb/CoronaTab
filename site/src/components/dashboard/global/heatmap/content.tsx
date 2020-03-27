d59b6ca6898bdf5a9c6490e6ccd76de034d16667
Will Viles <will@vil.es>
2020-03-27 18:24:18 +0000

    feat: adds loadingStatus to heatmap

--- site/src/components/dashboard/global/heatmap/content.tsx
+++ site/src/components/dashboard/global/heatmap/content.tsx
@@ -1,7 +1,7 @@
-import { Component, RefObject, createRef } from 'react'
+import { PureComponent, RefObject, createRef } from 'react'
 import { inject, observer } from 'mobx-react'
-import { DashboardPageStore } from '../../../../pages/dashboard.store'
-import { Feature, Map, View } from 'ol'
+import { DashboardPageStore, Place, LoadingStatus } from '../../../../pages/dashboard.store'
+import { Feature, Map, View, Overlay } from 'ol'
 import { Tile as TileLayer } from 'ol/layer'
 import WebGLPointsLayer from 'ol/layer/WebGLPoints'
 import XYZ from 'ol/source/XYZ'
@@ -12,11 +12,12 @@ import Point from 'ol/geom/Point'
 import { transform as transformProjection } from 'ol/proj'
 import { PlaceApi } from '../../../../utils/api/place'
 import tailwindConfig from '../../../../utils/tailwind'
+import OverlayPositioning from 'ol/OverlayPositioning'
+import { LoadingComponent } from '../../../loading'
 
 const {
   theme: {
     colors: {
-      lighter,
       red
     }
   }
@@ -28,37 +29,62 @@ export interface Props {
 }
 
 export interface State {
+  hoveredPlace?: Place
+  loadingStatus: LoadingStatus
 }
 
 @inject('pageStore')
 @observer
-export class DashboardGlobalHeatmapContentComponent extends Component<Props, State> {
+export class DashboardGlobalHeatmapContentComponent extends PureComponent<Props, State> {
+  state: State = {
+    hoveredPlace: null,
+    loadingStatus: LoadingStatus.IS_LOADING
+  }
+
   contentRef: RefObject<HTMLDivElement> = createRef()
   mapRef: RefObject<HTMLDivElement> = createRef()
+  mapTooltipRef: RefObject<HTMLDivElement> = createRef()
 
   map: Map
 
   componentDidMount () {
     document.addEventListener('click', this.onDocumentClick)
-    this.initMap()
+    this.fetchDataAndInitMap()
   }
 
-  initMap = async () => {
-    const { data: allPlaces } = await PlaceApi.query({ include: ['children'] })
+  fetchDataAndInitMap = async () => {
+    const places = await this.fetchData()
+    if (places) this.initMap(places)
+  }
 
-    const features = allPlaces
+  fetchData = async () => {
+    try {
+      this.setState({ loadingStatus: LoadingStatus.IS_LOADING })
+      const { data: allPlaces } = await PlaceApi.query({ include: ['children' ]})
+      this.setState({ loadingStatus: LoadingStatus.HAS_LOADED })
+      return allPlaces as Place[]
+    } catch (err) {
+      this.setState({ loadingStatus: LoadingStatus.HAS_ERRORED })
+    }
+  }
+
+  initMap = async (places: Place[]) => {
+    const features: Feature[] = places
       .filter(place => !place.children && Array.isArray(place.location?.coordinates))
       .map(place => new Feature({
         id: place.id,
+        place,
         geometry: new Point(transformProjection([place.location.coordinates[0], place.location.coordinates[1]], 'EPSG:4326', 'EPSG:3857')),
         value: place.latestData.cases
       }))
 
-    const maxCases = Math.max(...features.map((feature) => feature.get('value') as number))
+    const maxCases = Math.max(...features.map((feature: Feature) => feature.get('value') as number))
+
+    const featuresCollection = new Collection(features)
 
     const points = new WebGLPointsLayer({
       source: new VectorSource({
-        features: new Collection(features)
+        features: featuresCollection
       }),
       style: {
         symbol: {
@@ -88,7 +114,7 @@ export class DashboardGlobalHeatmapContentComponent extends Component<Props, Sta
           ]
         }
       },
-      disableHitDetection: true
+      disableHitDetection: false
     })
 
     const tiles = new TileLayer({
@@ -97,17 +123,42 @@ export class DashboardGlobalHeatmapContentComponent extends Component<Props, Sta
       })
     })
 
+    const tooltip = new Overlay({
+      element: this.mapTooltipRef.current,
+      offset: [10, 0],
+      positioning: OverlayPositioning.TOP_CENTER
+    })
+
     this.map = new Map({
       layers: [tiles, points],
+      overlays: [tooltip],
       target: this.mapRef.current,
       view: new View({
         center: [0, 0],
         zoom: 3
       })
     })
+
+    this.map.on('pointermove', evt => {
+      const { coordinate, pixel } = evt
+      const hoveredFeature = this.map.forEachFeatureAtPixel(pixel, feature => feature, {
+        hitTolerance: 10
+      })
+      this.mapRef.current.style.cursor = hoveredFeature ? 'pointer' : 'default'
+    })
+
+    this.map.on('singleclick', evt => {
+      const { coordinate, pixel } = evt
+      const hoveredPlace = this.map.forEachFeatureAtPixel(pixel, feature => feature.get('place'), {
+        hitTolerance: 10
+      })
+      this.setState({ hoveredPlace })
+      if (hoveredPlace) tooltip.setPosition(coordinate)
+    })
   }
 
   onDocumentClick = (e: MouseEvent) => {
+    if (this.state.loadingStatus === LoadingStatus.IS_LOADING) return
     const { current } = this.contentRef
     if (current && !current.contains(e.target as Node)) {
       this.props.onClose?.()
@@ -115,6 +166,7 @@ export class DashboardGlobalHeatmapContentComponent extends Component<Props, Sta
   }
 
   render () {
+    const { hoveredPlace, loadingStatus } = this.state
     return (
       <div
         ref={this.contentRef}
@@ -122,9 +174,53 @@ export class DashboardGlobalHeatmapContentComponent extends Component<Props, Sta
         style={{ height: '90vh' }}
       >
         <div
-          className="relative h-full bg-white rounded md:mx-6 cursor-default depth-lg overflow-hidden dashboard-spacer-x"
+          className="relative h-full bg-brand-dark text-white rounded md:mx-6 cursor-default depth-lg overflow-hidden dashboard-spacer-x"
         >
-          <div ref={this.mapRef} className="absolute inset-0" />
+          {
+            (() => {
+              switch (loadingStatus) {
+                case LoadingStatus.HAS_LOADED:
+                  return (<>
+                    <div ref={this.mapRef} className="absolute inset-0" />
+                    <div ref={this.mapTooltipRef} className="map-tooltip rounded-sm bg-white" style={{ display: hoveredPlace ? '' : 'none'}}>
+                      {
+                        hoveredPlace
+                          ? (
+                            <>
+                              <div className="px-2">
+                                <span>{hoveredPlace.name}</span>
+                              </div>
+                              <ul>
+                                <li>{hoveredPlace.latestData.cases}</li>
+                                <li>{hoveredPlace.latestData.deaths}</li>
+                                <li>{hoveredPlace.latestData.recovered}</li>
+                              </ul>
+                            </>
+                          )
+                          : ''
+                      }
+                    </div>
+                  </>)
+                case LoadingStatus.IS_LOADING:
+                  return (
+                    <div className="absolute inset-0 flex items-center justify-center">
+                      <LoadingComponent className="h-8" />
+                    </div>
+                  )
+                case LoadingStatus.HAS_ERRORED:
+                  return (
+                    <div className="absolute inset-0 flex items-center justify-center">
+                      <div className="text-center">
+                        <h2 className="font-bold text-lg mb-2">Errored loading map</h2>
+                        <button onClick={this.fetchDataAndInitMap} className="btn btn-white rounded px-2 py-1">
+                          Retry
+                        </button>
+                      </div>
+                    </div>
+                  )
+              }
+            })()
+          }
         </div>
       </div>
     )
