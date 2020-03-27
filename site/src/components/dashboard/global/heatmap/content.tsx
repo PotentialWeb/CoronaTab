3bcb692b0799ef78dcc676eaf19501d5e17b28c8
Will Viles <will@vil.es>
2020-03-27 17:14:59 +0000

    feat: implements map as WebGLPointsLayer rather than Heatmap

--- site/src/components/dashboard/global/heatmap/content.tsx
+++ site/src/components/dashboard/global/heatmap/content.tsx
@@ -2,13 +2,25 @@ import { Component, RefObject, createRef } from 'react'
 import { inject, observer } from 'mobx-react'
 import { DashboardPageStore } from '../../../../pages/dashboard.store'
 import { Feature, Map, View } from 'ol'
-import { Heatmap as HeatmapLayer, Tile as TileLayer } from 'ol/layer'
-import Stamen from 'ol/source/Stamen'
+import { Tile as TileLayer } from 'ol/layer'
+import WebGLPointsLayer from 'ol/layer/WebGLPoints'
+import XYZ from 'ol/source/XYZ'
 import VectorSource from 'ol/source/Vector'
 import Collection from 'ol/Collection'
+import { SymbolType } from 'ol/style/LiteralStyle'
 import Point from 'ol/geom/Point'
 import { transform as transformProjection } from 'ol/proj'
 import { PlaceApi } from '../../../../utils/api/place'
+import tailwindConfig from '../../../../utils/tailwind'
+
+const {
+  theme: {
+    colors: {
+      lighter,
+      red
+    }
+  }
+} = tailwindConfig
 
 export interface Props {
   onClose?: () => any
@@ -31,43 +43,69 @@ export class DashboardGlobalHeatmapContentComponent extends Component<Props, Sta
     this.initMap()
   }
 
-initMap = async () => {
-  const { data: allPlaces } = await PlaceApi.query({ include: ['children'] })
+  initMap = async () => {
+    const { data: allPlaces } = await PlaceApi.query({ include: ['children'] })
 
-  const features = allPlaces
-    .filter(place => !place.children && Array.isArray(place.location?.coordinates))
-    .map(place => new Feature({
-      id: place.id,
-      geometry: new Point(transformProjection([place.location.coordinates[0], place.location.coordinates[1]], 'EPSG:4326', 'EPSG:3857')),
-      value: place.latestData.cases
-    }))
+    const features = allPlaces
+      .filter(place => !place.children && Array.isArray(place.location?.coordinates))
+      .map(place => new Feature({
+        id: place.id,
+        geometry: new Point(transformProjection([place.location.coordinates[0], place.location.coordinates[1]], 'EPSG:4326', 'EPSG:3857')),
+        value: place.latestData.cases
+      }))
 
-  const maxCases = Math.max(...features.map((feature) => feature.get('value') as number))
+    const maxCases = Math.max(...features.map((feature) => feature.get('value') as number))
 
-  const heatmap = new HeatmapLayer({
-    source: new VectorSource({
-      features: new Collection(features)
-    }),
-    blur: 5,
-    radius: 3,
-    weight: (feature) => 0.8 + ((feature.get('value') / maxCases) * 0.2)
-  })
-
-  const tiles = new TileLayer({
-    source: new Stamen({
-      layer: 'terrain-background'
+    const points = new WebGLPointsLayer({
+      source: new VectorSource({
+        features: new Collection(features)
+      }),
+      style: {
+        symbol: {
+          symbolType: SymbolType.CIRCLE,
+          size: [
+            'interpolate',
+            ['exponential', 0.5],
+            ['get', 'value'],
+            0, 5,
+            maxCases, 64
+          ],
+          color: [
+            'interpolate',
+            ['exponential', 0.5],
+            ['get', 'value'],
+            0, red,
+            maxCases, 'red'
+          ],
+          rotateWithView: false,
+          offset: [0, 0],
+          opacity: [
+            'interpolate',
+            ['exponential', 0.5],
+            ['get', 'value'],
+            0, 0.66,
+            maxCases, 0.9
+          ]
+        }
+      },
+      disableHitDetection: true
     })
-  })
 
-  this.map = new Map({
-    layers: [tiles, heatmap],
-    target: this.mapRef.current,
-    view: new View({
-      center: [0, 0],
-      zoom: 3
+    const tiles = new TileLayer({
+      source: new XYZ({
+        url: 'https://api.mapbox.com/styles/v1/37x/ck8aei53a0zkd1inv3rkg7535/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiMzd4IiwiYSI6ImNqdzljOW1seDI0cGc0OXFyMmljZHY0M3kifQ.6agk0PoRjnclPax3ctVgnQ'
+      })
     })
-  })
-}
+
+    this.map = new Map({
+      layers: [tiles, points],
+      target: this.mapRef.current,
+      view: new View({
+        center: [0, 0],
+        zoom: 3
+      })
+    })
+  }
 
   onDocumentClick = (e: MouseEvent) => {
     const { current } = this.contentRef
@@ -81,7 +119,7 @@ initMap = async () => {
       <div
         ref={this.contentRef}
         className="container m-auto"
-        style={{ height: '90vh', maxWidth: '1280px' }}
+        style={{ height: '90vh' }}
       >
         <div
           className="relative h-full bg-white rounded md:mx-6 cursor-default depth-lg overflow-hidden dashboard-spacer-x"
